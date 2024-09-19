590fe2862f7e3816ee95301e7372ca87ce4afb62
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-09-19 11:54:56 -0500

    structure of the program

---
+++ src/searchAlgorithms/amplitudeSearch.go
@@ -0,0 +1,17 @@
+package searchAlgorithms
+
+import (
+  "time"
+)
+
+type AmplitudeSearch struct {
+  solutionPath [][]int
+}
+
+func (a *AmplitudeSearch) LookForGoal(e *enviroment) (solutionFound bool, expandenNodes, treeDepth, cost float32, timeExe time.Duration) {
+  // Generamos una percepción del entorno para ir expandiendo nodos
+
+  // Se modifica a.solutionPath que es el camino a seguir para llegar al objetivo
+  return
+}
+
--- src/searchAlgorithms/searchAlgorithms.go
+++ src/searchAlgorithms/searchAlgorithms.go
@@ -4,24 +4,34 @@ import (
   "time"
 )
 
+const (
+  wall = iota + 1
+  goal
+)
+
 // enviroment represents the enviroment where the agent is going to move
 type enviroment struct {
   matrix [][]int
-  totalGoal int
+  agentPosition, dogPosition, goalPosition [2]int
 }
 
-// SearchAlgorithm is the interface that the search algorithms must implement
-type SearchAgorithm interface {
-  LookForGoal(*enviroment) (solutionFound bool, expandenNodes, treeDepth, cost float32, timeExe time.Duration) 
+func NewEnviroment(matrix [][]int, totalGoal int) *enviroment {
+  // TODO: Extract positions of agent, dog, and goal
+  return &enviroment{
+    matrix: matrix,
+    agentPosition: [2]int{0, 0},
+    dogPosition: [2]int{0, 0},
+    goalPosition: [2]int{0, 0},
+  }
 }
 
 // agent represents the agent that is going to move in the enviroment
 type agent struct {
   x, y int
   searchAlgorithm SearchAgorithm
-  ambientPerception [4]int
+  ambientPerception [4]byte
+  dog bool
 }
-
 
 // MoveUp, MoveRight, MoveDown y MoveLeft son los actuadores del agente
 func (a *agent) MoveUp() {
@@ -37,9 +47,49 @@ func (a *agent) MoveLeft() {
   a.y--
 }
 
-type AmplitudeSearch struct {
+// generatePerception genera una percepción basada en el entorno
+func (a *agent) generatePerception(env enviroment) {
+  // Simulan los sensores
+  moveUp := a.x - 1
+  moveRight := a.y + 1
+  moveDown := a.x + 1
+  moveLeft := a.y - 1
+
+  const (
+    Up = iota
+    Right
+    Down
+    Left
+  )
+
+  // Se genera una percepción
+  if moveUp >= 0 && env.matrix[moveUp][a.y] != wall {
+    a.ambientPerception[Up] = 1 
+  } else {
+    a.ambientPerception[Up] = 0
+  }
+  if moveRight < len(env.matrix[0]) && env.matrix[a.x][moveRight] != wall {
+    a.ambientPerception[Right] = 1
+  } else {
+    a.ambientPerception[Right] = 0
+  }
+  if moveDown < len(env.matrix) && env.matrix[moveDown][a.y] != wall {
+    a.ambientPerception[Down] = 1 
+  } else {
+    a.ambientPerception[Down] = 0
+  }
+  if moveLeft >= 0 && env.matrix[a.x][moveLeft] != wall {
+    a.ambientPerception[Left] = 1 
+  } else {
+    a.ambientPerception[Left] = 0
+  }
 }
 
-func (a *AmplitudeSearch) LookForGoal(e *enviroment) (solutionFound bool, expandenNodes, treeDepth, cost float32, timeExe time.Duration) {
-  return
+func NewAgent(x, y int, searchAlgorithm SearchAgorithm) *agent {
+  return &agent{x, y, searchAlgorithm, [4]byte{}, false}
+}
+
+// SearchAlgorithm is the interface that the search algorithms must implement
+type SearchAgorithm interface {
+  LookForGoal(*enviroment) (solutionFound bool, expandenNodes, treeDepth, cost float32, timeExe time.Duration) 
 }
