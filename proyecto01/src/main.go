d612533bb1ad783969d9f11f4d81a10d2dc04ba0
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2024-10-09 23:18:40 -0500

    functions exposed for js implementation. not yet tested.

--- proyecto01/src/main.go
+++ proyecto01/src/main.go
@@ -2,27 +2,50 @@ package main
 
 import (
 	"log"
+	"syscall/js"
 
 	"github.com/Krud3/InteligenciaArtificial/src/game"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+var g *game.Game
+
 func main() {
 
 	matrixFileName := "Prueba1.txt"
 
-	game, err := game.NewGame(matrixFileName)
+	var err error
+	g, err = game.NewGame(matrixFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	game.SetCarPath("callDummy")
+	g.SetCarPath("callDummy")
 
 	// game.SetScene(matrixFileName)
 
+	// This will expose the SetPath function to JavaScript
+	js.Global().Set("setCarPath", js.FuncOf(setCarPath))
+	// This will expose the SetPath function to JavaScript
+	js.Global().Set("setCarPath", js.FuncOf(setScene))
+
 	ebiten.SetWindowSize(640, 640)
 	ebiten.SetWindowTitle("Searching Algorithms")
-	if err := ebiten.RunGame(game); err != nil {
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
+
+	select {}
+}
+
+func setCarPath(this js.Value, p []js.Value) interface{} {
+	path := p[0].String()
+	g.SetCarPath(path)
+	return nil
+}
+
+func setScene(this js.Value, p []js.Value) interface{} {
+	path := p[0].String()
+	g.SetScene(path)
+	return nil
 }
