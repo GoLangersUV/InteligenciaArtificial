147a0f02d9c0d1c1fd19b69a1a2808855c8436e3
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2024-11-21 23:10:08 -0500

    added svg images and implemented to the board

--- proyecto02/smart-horses/src/components/Board/Square.tsx
+++ proyecto02/smart-horses/src/components/Board/Square.tsx
@@ -12,11 +12,14 @@ interface SquareProps {
 const Square: React.FC<SquareProps> = ({ position, value, onClick, isSelected }) => {
   const isLightSquare = (position.row + position.col) % 2 === 0;
 
+  // Rutas de las imágenes según los valores
+  const pointImageSrc = value.points ? `/SVG/${value.points}.svg` : null;
+  const multiplierImageSrc = value.multiplier ? '/SVG/x2.svg' : null;
+
   return (
     <div
       className={`
         aspect-square w-full
-        flex items-center justify-center
         relative
         ${isLightSquare ? 'bg-sky-100' : 'bg-sky-300'}
         ${isSelected ? 'ring-2 ring-green-500 z-10' : ''}
@@ -25,23 +28,30 @@ const Square: React.FC<SquareProps> = ({ position, value, onClick, isSelected })
       `}
       onClick={onClick}
     >
+      {/* Contenedor de puntos y multiplicador */}
       <div className="absolute inset-0 flex items-center justify-center">
-        {value.points && (
-          <span className="font-bold text-xl text-black z-10">
-            {value.points}
-          </span>
+        {pointImageSrc && (
+          <img
+            src={pointImageSrc}
+            alt={`Points ${value.points}`}
+            className="w-full h-full object-contain z-10"
+          />
         )}
-        {value.multiplier && (
-          <span className="font-bold text-lg text-green-600 z-10">
-            x2
-          </span>
-        )}
-        {value.horse && (
-          <div className="z-20">
-            <Horse type={value.horse} />
-          </div>
+        {multiplierImageSrc && (
+          <img
+            src={multiplierImageSrc}
+            alt="x2 multiplier"
+            className="w-full h-full object-contain z-10"
+          />
         )}
       </div>
+
+      {/* Contenedor del caballo */}
+      {value.horse && (
+        <div className="absolute inset-0 z-20">
+          <Horse type={value.horse} />
+        </div>
+      )}
     </div>
   );
 };
